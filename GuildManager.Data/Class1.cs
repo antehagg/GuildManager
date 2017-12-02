03fc5c179257501fa0a700fafe318dd21e5caefa
Andreas Hägglund <ante.hagglund@gmail.com>
2017-12-02 12:34:14 +0100

    First commit
    First commit

---
+++ GuildManager.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GuildManager.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ GuildManager.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuildManager.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
